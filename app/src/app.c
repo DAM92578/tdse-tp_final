9129675bc1215f38d2afc289afcc38b5edcfefd8
DAM92578 <damuller@fi.uba.ar>
2024-12-15 18:03:17 -0300

    first commit

--- app/src/app.c
+++ app/src/app.c
@@ -47,6 +47,7 @@
 #include "board.h"
 #include "task_sensor.h"
 #include "task_menu.h"
+#include "task_adc.h"
 #include "task_actuator.h"
 #include "task_adc.h"
 
@@ -74,8 +75,9 @@ typedef struct {
 const task_cfg_t task_cfg_list[]	= {
 		{task_sensor_init,	task_sensor_update, 	NULL},
 		{task_menu_init,	task_menu_update, 		NULL},
+		{task_adc_init, task_adc_update, NULL},
 		{task_actuator_init,	task_actuator_update, 	NULL},
-		{task_adc_init, task_adc_update, NULL}
+	
 };
 
 #define TASK_QTY	(sizeof(task_cfg_list)/sizeof(task_cfg_t))
